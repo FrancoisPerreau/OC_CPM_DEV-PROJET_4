757bfa9d8eb51ac334d5157f16b4376558e22af1
FrancoisPerreau <f.perreau@orange.fr>
2019-02-11 21:45:16 +0100

    Passage de données, de la home vers la vue order (via $_SESSION)

--- src/CL/TicketingBundle/Controller/TicketingController.php
+++ src/CL/TicketingBundle/Controller/TicketingController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 // use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\HttpFoundation\Response;
 
 use CL\TicketingBundle\Form\TicketDateChoiceType;
 
@@ -22,11 +23,26 @@ class TicketingController extends Controller
 
       if ($form->isValid() && $form->isSubmitted())
       {
-          dump($_POST);die;
+          $_SESSION['data'] = $form->getData();
+          // dump($_SESSION['data']);die;
+          return $this->redirectToRoute('order_regitration');
       }
 
       return $this->render('@CLTicketing/Ticketing/homepage.html.twig',[
           'form' => $form->createView(),
       ]);
+    }
+
+
+    /**
+     * @Route("/order", name="order_regitration")
+     */
+    public function orderAction(Request $request)
+    {
+        $data = $_SESSION['data'];
+        // $nbTicket = $request->get('TicketNb');
+        return $this->render('@CLTicketing/Ticketing/order.html.twig', [
+            'data' => $data
+        ]);
     }
 }
